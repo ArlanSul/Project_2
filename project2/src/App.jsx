54fae9a7c8230c05f14fcea507f68a4b6124fa6f
Arlan S <36273736+ArlanSul@users.noreply.github.com>
2026-03-11 01:42:03 -0400

    header with button add

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -3,7 +3,15 @@ import './App.css'
 
 function App() {
   <div className="App">
-    
+    <div className="header">
+        <h1>Ultimate Trivia!</h1>
+        <p>Test your general knowledge with these flashcards.</p>
+        <h5>Total Cards: </h5>
+      </div>
+
+      <button className="next-btn">
+        Next Card ➔
+      </button>
   </div>  
 }
 
